b6d1138d1ef7fe292099084c2e7e38fad9f290a7
Bugi Görtz <bugi@joboland.de>
2018-07-04 14:27:20 +0200

    raised version

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array (
     ),
   ),
   'state' => 'stable',
-  'version' => '1.1.2',
+  'version' => '1.1.3',
   'clearCacheOnLoad' => 0,
   'createDirs' => '',
   'uploadfolder' => 0,
