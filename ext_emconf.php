73bc48fb65b1ab2c027dce67c44a37218a30c7d3
Bugi Görtz <bugi@joboland.de>
2018-07-04 14:13:54 +0200

    raised max TYPO3-Version

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '7.6.0-8.6.99',
+      'typo3' => '7.6.0-8.9.99',
     ),
     'conflicts' => 
     array (
